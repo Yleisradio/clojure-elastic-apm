6f899d7eb4009c8e5d39f9cfeefa815882decb22
Jami Karvanen <jami.karvanen@yle.fi>
2021-12-10 21:22:44 +0200

    Upgrade elastic-apm-agent to 1.28.1 (fixes log4j vulnerability) (#7)

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.2"]]
-  :profiles {:dev {:jvm-opts ["-javaagent:lib/elastic-apm-agent-1.20.0.jar"
+  :profiles {:dev {:jvm-opts ["-javaagent:lib/elastic-apm-agent-1.28.1.jar"
                               "-Delastic.apm.service_name=test-service"
                               "-Delastic.apm.application_packages=clojure-elastic-apm"
                               "-Delastic.apm.server_urls=http://localhost:8200"
                               "-Delastic.apm.metrics_interval=1s"]
                    :dependencies [[clj-http "3.10.1"]
                                   [cheshire "5.10.0"]]}
-             :provided {:dependencies [[co.elastic.apm/apm-agent-api "1.20.0"]]}})
+             :provided {:dependencies [[co.elastic.apm/apm-agent-api "1.28.1"]]}})
