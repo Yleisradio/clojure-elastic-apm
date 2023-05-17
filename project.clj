4d0b7b3c42cb0cff5087d93fe6862985335a4759
Ari Paasonen <ari.paasonen@wunderdog.fi>
2023-05-17 10:47:33 +0900

    Update dependencies, bump version to 0.7.1
    Clojure 1.10.2 -> 1.11.1
    elastic-apm-agent 1.28.1 -> 1.38.0
    clj-http 3.10.1 -> 3.12.3
    cheshire 5.10.0 -> 5.11.0

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject clojure-elastic-apm "0.7.0"
+(defproject clojure-elastic-apm "0.7.1"
   :description "Clojure wrapper for Elastic APM Java Agent"
   :url "https://github.com/Yleisradio/clojure-elastic-apm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.2"]]
-  :profiles {:dev {:jvm-opts ["-javaagent:lib/elastic-apm-agent-1.28.1.jar"
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :profiles {:dev {:jvm-opts ["-javaagent:lib/elastic-apm-agent-1.38.0.jar"
                               "-Delastic.apm.service_name=test-service"
                               "-Delastic.apm.application_packages=clojure-elastic-apm"
                               "-Delastic.apm.server_urls=http://localhost:8200"
                               "-Delastic.apm.metrics_interval=1s"]
-                   :dependencies [[clj-http "3.10.1"]
-                                  [cheshire "5.10.0"]]}
-             :provided {:dependencies [[co.elastic.apm/apm-agent-api "1.28.1"]]}})
+                   :dependencies [[clj-http "3.12.3"]
+                                  [cheshire "5.11.0"]]}
+             :provided {:dependencies [[co.elastic.apm/apm-agent-api "1.38.0"]]}})
